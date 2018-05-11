23a483ec1022280e11b983d707f2cde5c8d3884c
Glenn Eggeton <geggleton@mobials.com>
2018-05-11 19:26:08 -0400

    Update

--- src/components/FancyPage.vue
+++ src/components/FancyPage.vue
@@ -6,175 +6,175 @@
 
             <div class="column is-2">
                 <a href="https://www.cryptokitties.co/kitty/1" target="_blank">
-                    <flippy-kitty src="https://storage.googleapis.com/ck-kitty-image/0x06012c8cf97bead5deae237070f9587f8e7a266d/1.png"></flippy-kitty>
+                    <flippy-kitty holo="true" src="https://storage.googleapis.com/ck-kitty-image/0x06012c8cf97bead5deae237070f9587f8e7a266d/1.png"></flippy-kitty>
                 </a>
             </div>
 
             <div class="column is-2">
                 <a href="https://www.cryptokitties.co/marketplace?search=fancy%3Aswish" target="_blank">
-                    <img class="image" src="https://storage.googleapis.com/ck-kitty-image/0x06012c8cf97bead5deae237070f9587f8e7a266d/744219.png">
+                    <flippy-kitty  src="https://storage.googleapis.com/ck-kitty-image/0x06012c8cf97bead5deae237070f9587f8e7a266d/744219.png"></flippy-kitty>
                 </a>
             </div>
 
             <div class="column is-2">
                 <a href="https://www.cryptokitties.co/marketplace?search=fancy%3Aflutterbee" target="_blank">
-                    <img class="image" src="https://storage.googleapis.com/ck-kitty-image/0x06012c8cf97bead5deae237070f9587f8e7a266d/719874.png">
+                    <flippy-kitty  src="https://storage.googleapis.com/ck-kitty-image/0x06012c8cf97bead5deae237070f9587f8e7a266d/719874.png"></flippy-kitty>
                 </a>
             </div>
 
             <div class="column is-2">
                 <a href="https://www.cryptokitties.co/marketplace?search=fancy%3Avernon" target="_blank">
-                    <img class="image" src="https://storage.googleapis.com/ck-kitty-image/0x06012c8cf97bead5deae237070f9587f8e7a266d/696398.png">
+                    <flippy-kitty   src="https://storage.googleapis.com/ck-kitty-image/0x06012c8cf97bead5deae237070f9587f8e7a266d/696398.png"></flippy-kitty>
                 </a>
             </div>
 
             <div class="column is-2">
                 <a href="https://www.cryptokitties.co/marketplace?search=fancy%3Aberry" target="_blank">
-                    <img class="image" src="https://storage.googleapis.com/ck-kitty-image/0x06012c8cf97bead5deae237070f9587f8e7a266d/627082.png">
+                    <flippy-kitty   src="https://storage.googleapis.com/ck-kitty-image/0x06012c8cf97bead5deae237070f9587f8e7a266d/627082.png"></flippy-kitty>
                 </a>
             </div>
 
             <div class="column is-2">
                 <a href="https://www.cryptokitties.co/marketplace?search=fancy%3Aknightkitty" target="_blank">
-                    <img class="image" src="https://storage.googleapis.com/ck-kitty-image/0x06012c8cf97bead5deae237070f9587f8e7a266d/104.png">
+                    <flippy-kitty   src="https://storage.googleapis.com/ck-kitty-image/0x06012c8cf97bead5deae237070f9587f8e7a266d/104.png"></flippy-kitty>
                 </a>
             </div>
 
             <div class="column is-2">
                 <a href="https://www.cryptokitties.co/marketplace?search=fancy%3A汪星爷" target="_blank">
-                    <img class="image" src="https://storage.googleapis.com/ck-kitty-image/0x06012c8cf97bead5deae237070f9587f8e7a266d/586194.png">
+                    <flippy-kitty   src="https://storage.googleapis.com/ck-kitty-image/0x06012c8cf97bead5deae237070f9587f8e7a266d/586194.png"></flippy-kitty>
                 </a>
             </div>
 
             <div class="column is-2">
                 <a href="https://www.cryptokitties.co/marketplace?search=fancy%3A咚咚锵" target="_blank">
-                    <img class="image" src="https://storage.googleapis.com/ck-kitty-image/0x06012c8cf97bead5deae237070f9587f8e7a266d/596826.png">
+                    <flippy-kitty   src="https://storage.googleapis.com/ck-kitty-image/0x06012c8cf97bead5deae237070f9587f8e7a266d/596826.png"></flippy-kitty>
                 </a>
             </div>
 
             <div class="column is-2">
                 <a href="https://www.cryptokitties.co/marketplace?search=fancy%3Apussforprogress" target="_blank">
-                    <img class="image" src="https://storage.googleapis.com/ck-kitty-image/0x06012c8cf97bead5deae237070f9587f8e7a266d/617794.png">
+                    <flippy-kitty   src="https://storage.googleapis.com/ck-kitty-image/0x06012c8cf97bead5deae237070f9587f8e7a266d/617794.png"></flippy-kitty>
                 </a>
             </div>
 
             <div class="column is-2">
                 <a href="https://www.cryptokitties.co/marketplace?search=fancy%3A红包喵" target="_blank">
-                    <img class="image" src="https://storage.googleapis.com/ck-kitty-image/0x06012c8cf97bead5deae237070f9587f8e7a266d/623578.png">
+                    <flippy-kitty   src="https://storage.googleapis.com/ck-kitty-image/0x06012c8cf97bead5deae237070f9587f8e7a266d/623578.png"></flippy-kitty>
                 </a>
             </div>
 
             <div class="column is-2">
                 <a href="https://www.cryptokitties.co/marketplace?search=fancy%3Atabby" target="_blank">
-                    <img class="image" src="https://storage.googleapis.com/ck-kitty-image/0x06012c8cf97bead5deae237070f9587f8e7a266d/572197.png">
+                    <flippy-kitty   src="https://storage.googleapis.com/ck-kitty-image/0x06012c8cf97bead5deae237070f9587f8e7a266d/572197.png"></flippy-kitty>
                 </a>
             </div>
 
             <div class="column is-2">
                 <a href="https://www.cryptokitties.co/marketplace?search=fancy%3AYuriCatsuki" target="_blank">
-                    <img class="image" src="https://storage.googleapis.com/ck-kitty-image/0x06012c8cf97bead5deae237070f9587f8e7a266d/551150.png">
+                    <flippy-kitty   src="https://storage.googleapis.com/ck-kitty-image/0x06012c8cf97bead5deae237070f9587f8e7a266d/551150.png"></flippy-kitty>
                 </a>
             </div>
 
             <div class="column is-2">
                 <a href="https://www.cryptokitties.co/marketplace?search=fancy%3AMisterPurrfect" target="_blank">
-                    <img class="image" src="https://storage.googleapis.com/ck-kitty-image/0x06012c8cf97bead5deae237070f9587f8e7a266d/554051.png">
+                    <flippy-kitty   src="https://storage.googleapis.com/ck-kitty-image/0x06012c8cf97bead5deae237070f9587f8e7a266d/554051.png"></flippy-kitty>
                 </a>
             </div>
 
             <div class="column is-2">
                 <a href="https://www.cryptokitties.co/marketplace?search=fancy%3Aearnie" target="_blank">
-                    <img class="image" src="https://storage.googleapis.com/ck-kitty-image/0x06012c8cf97bead5deae237070f9587f8e7a266d/556220.png">
+                    <flippy-kitty   src="https://storage.googleapis.com/ck-kitty-image/0x06012c8cf97bead5deae237070f9587f8e7a266d/556220.png"></flippy-kitty>
                 </a>
             </div>
 
             <div class="column is-2">
                 <a href="https://www.cryptokitties.co/kitty/500000" target="_blank">
-                    <img class="image" src="https://storage.googleapis.com/ck-kitty-image/0x06012c8cf97bead5deae237070f9587f8e7a266d/500000.png">
+                    <flippy-kitty   src="https://storage.googleapis.com/ck-kitty-image/0x06012c8cf97bead5deae237070f9587f8e7a266d/500000.png"></flippy-kitty>
                 </a>
             </div>
 
             <div class="column is-2">
                 <a href="https://www.cryptokitties.co/marketplace?search=fancy%3AMomo-chan" target="_blank">
-                    <img class="image" src="https://storage.googleapis.com/ck-kitty-image/0x06012c8cf97bead5deae237070f9587f8e7a266d/574582.png">
+                    <flippy-kitty   src="https://storage.googleapis.com/ck-kitty-image/0x06012c8cf97bead5deae237070f9587f8e7a266d/574582.png"></flippy-kitty>
                 </a>
             </div>
 
             <div class="column is-2">
                 <a href="https://www.cryptokitties.co/marketplace?search=fancy%3ANegato" target="_blank">
-                    <img class="image" src="https://storage.googleapis.com/ck-kitty-image/0x06012c8cf97bead5deae237070f9587f8e7a266d/558202.png">
+                    <flippy-kitty   src="https://storage.googleapis.com/ck-kitty-image/0x06012c8cf97bead5deae237070f9587f8e7a266d/558202.png"></flippy-kitty>
                 </a>
             </div>
 
             <div class="column is-2">
                 <a href="https://www.cryptokitties.co/marketplace?search=fancy%3AStitches" target="_blank">
-                    <img class="image" src="https://storage.googleapis.com/ck-kitty-image/0x06012c8cf97bead5deae237070f9587f8e7a266d/562940.png">
+                    <flippy-kitty   src="https://storage.googleapis.com/ck-kitty-image/0x06012c8cf97bead5deae237070f9587f8e7a266d/562940.png"></flippy-kitty>
                 </a>
             </div>
 
             <div class="column is-2">
                 <a href="https://www.cryptokitties.co/marketplace?search=fancy%3APhuZiqaat" target="_blank">
-                    <img class="image" src="https://storage.googleapis.com/ck-kitty-image/0x06012c8cf97bead5deae237070f9587f8e7a266d/572389.png">
+                    <flippy-kitty   src="https://storage.googleapis.com/ck-kitty-image/0x06012c8cf97bead5deae237070f9587f8e7a266d/572389.png"></flippy-kitty>
                 </a>
             </div>
 
             <div class="column is-2">
                 <a href="https://www.cryptokitties.co/marketplace?search=fancy%3ASantaClaws" target="_blank">
-                    <img class="image" src="https://storage.googleapis.com/ck-kitty-image/0x06012c8cf97bead5deae237070f9587f8e7a266d/549629.png">
+                    <flippy-kitty   src="https://storage.googleapis.com/ck-kitty-image/0x06012c8cf97bead5deae237070f9587f8e7a266d/549629.png"></flippy-kitty>
                 </a>
             </div>
 
             <div class="column is-2">
                 <a href="https://www.cryptokitties.co/marketplace?search=fancy%3AMistletoe" target="_blank">
-                    <img class="image" src="https://storage.googleapis.com/ck-kitty-image/0x06012c8cf97bead5deae237070f9587f8e7a266d/553850.png">
+                    <flippy-kitty   src="https://storage.googleapis.com/ck-kitty-image/0x06012c8cf97bead5deae237070f9587f8e7a266d/553850.png"></flippy-kitty>
                 </a>
             </div>
 
             <div class="column is-2">
                 <a href="https://www.cryptokitties.co/marketplace?search=fancy%3AShipCat" target="_blank">
-                    <img class="image" src="https://storage.googleapis.com/ck-kitty-image/0x06012c8cf97bead5deae237070f9587f8e7a266d/567966.png">
+                    <flippy-kitty   src="https://storage.googleapis.com/ck-kitty-image/0x06012c8cf97bead5deae237070f9587f8e7a266d/567966.png"></flippy-kitty>
                 </a>
             </div>
 
             <div class="column is-2">
                 <a href="https://www.cryptokitties.co/marketplace?search=fancy%3ADuCat" target="_blank">
-                    <img class="image" src="https://storage.googleapis.com/ck-kitty-image/0x06012c8cf97bead5deae237070f9587f8e7a266d/657713.png">
+                    <flippy-kitty   src="https://storage.googleapis.com/ck-kitty-image/0x06012c8cf97bead5deae237070f9587f8e7a266d/657713.png"></flippy-kitty>
                 </a>
             </div>
 
             <div class="column is-2">
                 <a href="https://www.cryptokitties.co/marketplace?search=fancy%3ADracula" target="_blank">
-                    <img class="image" src="https://storage.googleapis.com/ck-kitty-image/0x06012c8cf97bead5deae237070f9587f8e7a266d/577945.png">
+                    <flippy-kitty   src="https://storage.googleapis.com/ck-kitty-image/0x06012c8cf97bead5deae237070f9587f8e7a266d/577945.png"></flippy-kitty>
                 </a>
             </div>
 
             <div class="column is-2">
                 <a href="https://www.cryptokitties.co/marketplace?search=fancy%3A旺财汪" target="_blank">
-                    <img class="image" src="https://storage.googleapis.com/ck-kitty-image/0x06012c8cf97bead5deae237070f9587f8e7a266d/1826.png">
+                    <flippy-kitty   src="https://storage.googleapis.com/ck-kitty-image/0x06012c8cf97bead5deae237070f9587f8e7a266d/1826.png"></flippy-kitty>
                 </a>
             </div>
 
             <div class="column is-2">
                 <a href="https://www.cryptokitties.co/kitty/230" target="_blank">
-                    <img class="image" src="https://storage.googleapis.com/ck-kitty-image/0x06012c8cf97bead5deae237070f9587f8e7a266d/230.png">
+                    <flippy-kitty   src="https://storage.googleapis.com/ck-kitty-image/0x06012c8cf97bead5deae237070f9587f8e7a266d/230.png"></flippy-kitty>
                 </a>
             </div>
 
             <div class="column is-2">
                 <a href="https://www.cryptokitties.co/kitty/130" target="_blank">
-                    <img class="image" src="https://storage.googleapis.com/ck-kitty-image/0x06012c8cf97bead5deae237070f9587f8e7a266d/130.png">
+                    <flippy-kitty   src="https://storage.googleapis.com/ck-kitty-image/0x06012c8cf97bead5deae237070f9587f8e7a266d/130.png"></flippy-kitty>
                 </a>
             </div>
 
             <div class="column is-2">
                 <a href="https://www.cryptokitties.co/kitty/330" target="_blank">
-                    <img class="image" src="https://storage.googleapis.com/ck-kitty-image/0x06012c8cf97bead5deae237070f9587f8e7a266d/330.png">
+                    <flippy-kitty   src="https://storage.googleapis.com/ck-kitty-image/0x06012c8cf97bead5deae237070f9587f8e7a266d/330.png"></flippy-kitty>
                 </a>
             </div>
 
             <div class="column is-2">
                 <a href="https://www.cryptokitties.co/marketplace?search=fancy%3Abugcat" target="_blank">
-                    <img class="image" src="https://storage.googleapis.com/ck-kitty-image/0x06012c8cf97bead5deae237070f9587f8e7a266d/103.png">
+                    <flippy-kitty  src="https://storage.googleapis.com/ck-kitty-image/0x06012c8cf97bead5deae237070f9587f8e7a266d/103.png"></flippy-kitty>
                 </a>
             </div>
         </div>
@@ -198,5 +198,110 @@
 </script>
 
 <style>
+    .flip-container {
+        -webkit-perspective: 1000;
+        -moz-perspective: 1000;
+        -o-perspective: 1000;
+        perspective: 1000;
+    }
 
+    .flip-container:hover .flipper,
+    .flip-container.hover .flipper {
+        -webkit-transform: rotateY(180deg);
+        -moz-transform: rotateY(180deg);
+        -o-transform: rotateY(180deg);
+        transform: rotateY(180deg);
+    }
+    .flipper {
+        -webkit-transition: 0.6s;
+        -webkit-transform-style: preserve-3d;
+
+        -moz-transition: 0.6s;
+        -moz-transform-style: preserve-3d;
+
+        -o-transition: 0.6s;
+        -o-transform-style: preserve-3d;
+
+        transition: 0.6s;
+        transform-style: preserve-3d;
+
+        max-height: 140px;
+        height: 140px;
+        max-width: 140px;
+
+        display: block;
+
+        border: #1c1c1c solid 1px;
+    }
+
+    .front, .back {
+        -webkit-backface-visibility: hidden;
+        -moz-backface-visibility: hidden;
+        -o-backface-visibility: hidden;
+        backface-visibility: hidden;
+
+        position: absolute;
+        top: 0;
+        left: 0;
+    }
+
+    .back {
+        -webkit-transform: rotateY(180deg);
+        -moz-transform: rotateY(180deg);
+        -o-transform: rotateY(180deg);
+        transform: rotateY(180deg);
+
+        z-index: 10;
+
+        background: linear-gradient(-30deg, #1c3aa7, #b939a3, #26a87c, #76ab4d);
+        background-size: 400% 400%;
+        -webkit-animation: Gradient 4s ease infinite;
+        -moz-animation: Gradient 4s ease infinite;
+        animation: Gradient 4s ease infinite;
+    }
+
+    .holographic-background {
+        color: #fff;
+        background: linear-gradient( -45deg, #ffcf5b, #ff99a8, #c8daff, #bbffef);
+        background-size: 400% 400%;
+        -webkit-animation: Gradient 4s ease infinite;
+        -moz-animation: Gradient 4s ease infinite;
+        animation: Gradient 4s ease infinite;
+    }
+
+    @-webkit-keyframes Gradient {
+        0% {
+            background-position: 0% 50%
+        }
+        50% {
+            background-position: 100% 50%
+        }
+        100% {
+            background-position: 0% 50%
+        }
+    }
+
+    @-moz-keyframes Gradient {
+        0% {
+            background-position: 0% 50%
+        }
+        50% {
+            background-position: 100% 50%
+        }
+        100% {
+            background-position: 0% 50%
+        }
+    }
+
+    @keyframes Gradient {
+        0% {
+            background-position: 0% 50%
+        }
+        50% {
+            background-position: 100% 50%
+        }
+        100% {
+            background-position: 0% 50%
+        }
+    }
 </style>
--- src/components/FlippyKitty.vue
+++ src/components/FlippyKitty.vue
@@ -2,7 +2,7 @@
     <div class="flip-container" ontouchstart="this.classList.toggle('hover');">
         <div class="flipper">
             <div class="front">
-                <img class="image holographic-background" v-bind:src="src"/>
+                <img class="image" v-bind:src="src" v-bind:class="{ 'holographic-background' : holo }"/>
             </div>
             <div class="back">
                 <img class="image" src="/assets/KB-Logo.png">
@@ -15,105 +15,12 @@
 export default {
     name : 'FlippyKitty',
     props : [
-        'src'
+        'src',
+        'holo'
     ]
 }
 </script>
 
-<style scoped>
-    .flip-container {
-        -webkit-perspective: 1000;
-        -moz-perspective: 1000;
-        -o-perspective: 1000;
-        perspective: 1000;
-    }
+<style>
 
-    .flip-container:hover .flipper,
-    .flip-container.hover .flipper {
-        -webkit-transform: rotateY(180deg);
-        -moz-transform: rotateY(180deg);
-        -o-transform: rotateY(180deg);
-        transform: rotateY(180deg);
-    }
-    .flipper {
-        -webkit-transition: 0.6s;
-        -webkit-transform-style: preserve-3d;
-
-        -moz-transition: 0.6s;
-        -moz-transform-style: preserve-3d;
-
-        -o-transition: 0.6s;
-        -o-transform-style: preserve-3d;
-
-        transition: 0.6s;
-        transform-style: preserve-3d;
-
-        position: relative;
-    }
-
-    .front, .back {
-        -webkit-backface-visibility: hidden;
-        -moz-backface-visibility: hidden;
-        -o-backface-visibility: hidden;
-        backface-visibility: hidden;
-
-        position: absolute;
-        top: 0;
-        left: 0;
-    }
-
-    .back {
-        -webkit-transform: rotateY(180deg);
-        -moz-transform: rotateY(180deg);
-        -o-transform: rotateY(180deg);
-        transform: rotateY(180deg);
-
-        z-index: 10;
-        background-color: white;
-    }
-
-    .holographic-background {
-        color: #fff;
-        background: linear-gradient( -45deg, #ffcf5b, #ff99a8, #c8daff, #bbffef);
-        background-size: 400% 400%;
-        -webkit-animation: Gradient 4s ease infinite;
-        -moz-animation: Gradient 4s ease infinite;
-        animation: Gradient 4s ease infinite;
-    }
-
-    @-webkit-keyframes Gradient {
-        0% {
-            background-position: 0% 50%
-        }
-        50% {
-            background-position: 100% 50%
-        }
-        100% {
-            background-position: 0% 50%
-        }
-    }
-
-    @-moz-keyframes Gradient {
-        0% {
-            background-position: 0% 50%
-        }
-        50% {
-            background-position: 100% 50%
-        }
-        100% {
-            background-position: 0% 50%
-        }
-    }
-
-    @keyframes Gradient {
-        0% {
-            background-position: 0% 50%
-        }
-        50% {
-            background-position: 100% 50%
-        }
-        100% {
-            background-position: 0% 50%
-        }
-    }
 </style>
